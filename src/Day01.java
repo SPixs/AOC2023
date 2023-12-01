e45e076248c8c46145707373a9493bd46d21a21e
Seb <sebastien.mametz@gmail.com>
2023-12-01 09:08:53 +0100

    Day 1 setup

---
+++ src/Day01.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Day01 {
+
+	public static void main(String[] args) throws IOException {
+		List<String> lines = Files.readAllLines(Path.of("input_day01.txt"));
+
+		
+	}
+
+}
